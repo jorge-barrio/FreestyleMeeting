0ce5f1547fd510e3409dc28960f748db65ec7cbe
luis <luitome>
2020-11-08 00:51:25 +0100

    2 vistas Pista y InicioEntrenamiento

---
+++ app/src/main/java/com/example/freestylemeeting/ListaPistaActivity.java
@@ -0,0 +1,13 @@
+package com.example.freestylemeeting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaPistaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pista);
+    }
+}
