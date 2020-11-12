49bacbb65d5e020111400d64c65eacbc21da3a6a
Luis <luis111222333444@gmail.com>
2020-11-12 18:42:58 +0100

    Vista ReservarMaterial, ListaPista y unas modificaciones

--- app/src/main/java/com/example/freestylemeeting/PistaActivity.java
+++ app/src/main/java/com/example/freestylemeeting/PistaActivity.java
@@ -1,14 +1,13 @@
 package com.example.freestylemeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class ListaPistaActivity extends AppCompatActivity {
+public class PistaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_pista);
+        setContentView(R.layout.activity_pista);
     }
 }
--- app/src/main/java/com/example/freestylemeeting/ReservarMaterialActivity.java
+++ app/src/main/java/com/example/freestylemeeting/ReservarMaterialActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListaPistaActivity extends AppCompatActivity {
+public class ReservarMaterialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_pista);
+        setContentView(R.layout.activity_reservar_material);
     }
 }
