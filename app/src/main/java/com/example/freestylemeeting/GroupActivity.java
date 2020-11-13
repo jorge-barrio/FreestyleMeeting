568de8ff0fd418bc74af26825255964c867dfebb
raul jimenez bores <raul.jimenez.bores@alumnos.uva.es>
2020-11-13 17:05:07 +0100

    boton accede a crear grupo

--- app/src/main/java/com/example/freestylemeeting/GroupActivity.java
+++ app/src/main/java/com/example/freestylemeeting/GroupActivity.java
@@ -2,7 +2,10 @@ package com.example.freestylemeeting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GroupActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class GroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+        Button botonCrearGrupo = (Button) findViewById(R.id.crearGrupo);
+        botonCrearGrupo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GroupActivity.this,CrearGruposActivity.class));
+            }
+        });
+        
+
     }
 }
